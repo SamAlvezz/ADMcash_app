1ce5aff50a04dcf2d388975d87bd0903f4700db4
Samuel Alves <samuelalvesdiasdasilvasadds@gmail.com>
2023-10-19 13:14:11 -0300

    gráfico teste adicionado

--- adm/src/pages/Graficos/Graficos.jsx
+++ adm/src/pages/Graficos/Graficos.jsx
@@ -1,19 +1,40 @@
-import react from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native"
-import { PieChart } from "react-native-svg-charts"
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView, Dimensions } from "react-native";
+import { PieChart } from "react-native-chart-kit";
+
+const screenWidth = Dimensions.get('window').width;
+
+const data = [
+  { name: 'A', population: 50, color: '#1abc9c' },
+  { name: 'B', population: 30, color: '#3498db' },
+  { name: 'C', population: 20, color: '#e74c3c' },
+];
+
+const chartConfig = {
+  backgroundGradientFrom: '#1E2923',
+  backgroundGradientTo: '#08130D',
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+};
 
 export default function Graficos() {
   return (
-    
-    <View style = { styles.container } texto = { "Graficos"} >
-      <SafeAreaView  style={styles.containerHeader}>
-        <View>
-           <Text style={styles.mensagem}>Graficos</Text>
-        </View>,
-        </SafeAreaView>
-      <Text style={styles.text} >Pagina Graficos</Text>
- </View>
-  )
+    <View style={styles.container}>
+      <SafeAreaView style={styles.containerHeader}>
+        <Text style={styles.textgraficos}>Gráficos</Text>
+      </SafeAreaView>
+      <Text style={styles.text}>Janeiro 2023</Text>
+      <PieChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={chartConfig}
+        accessor="population"
+        backgroundColor="transparent"
+        center={[10, 50]}
+        
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -25,7 +46,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFF",
   },
   text: {
-
+    width: 188,
+    height: 27,
+    fontSize: 17,
+    color: '#3F96E7',
+    textAlign: 'center'
   },
   containerHeader: {
     flexDirection: 'row',
@@ -33,17 +58,14 @@ const styles = StyleSheet.create({
     width: 360,
     height: 80,
     alignItems: 'center',
-    paddingStart: '20%'
-
+    paddingLeft: '20%',
   },
-  mensagem: {
+  textgraficos: {
     width: 188,
     height: 27,
     flexShrink: 0,
     fontSize: 20,
     color: '#fff',
     textAlign: 'center',
-    teste
-
   },
-})
+});

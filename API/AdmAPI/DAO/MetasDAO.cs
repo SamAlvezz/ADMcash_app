71e80d6fd701df7cfbd521905dfe3dd248d9f919
TiagoChaves2005 <140762745+TiagoChaves2005@users.noreply.github.com>
2023-12-07 14:35:16 -0300

    ACRESCENTAR DATA DE METAS

--- API/AdmAPI/DAO/MetasDAO.cs
+++ API/AdmAPI/DAO/MetasDAO.cs
@@ -1,5 +1,6 @@
 ﻿using AdmAPI.DTO;
 using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 
 namespace AdmAPI.DAO
@@ -25,6 +26,7 @@ namespace AdmAPI.DAO
                 meta.COD_MT = int.Parse(dataReader["COD_MT"].ToString());
                 meta.NOME_MT = dataReader["NOME_MT"].ToString();
                 meta.VALOR_MT = float.Parse(dataReader["VALOR_MT"].ToString());
+                meta.DATA_ALCANCAR = DateTime.Parse(dataReader["DATA_ALCANCAR"].ToString());
 
                 metas.Add(meta);
             }
@@ -51,7 +53,8 @@ namespace AdmAPI.DAO
                 meta.COD_MT = int.Parse(dataReader["COD_MT"].ToString());
                 meta.NOME_MT = dataReader["NOME_MT"].ToString();
                 meta.VALOR_MT = float.Parse(dataReader["VALOR_MT"].ToString());
-                
+                meta.DATA_ALCANCAR = DateTime.Parse(dataReader["DATA_ALCANCAR"].ToString());
+
                 metas.Add(meta);
             }
             conexao.Close();
@@ -61,31 +64,35 @@ namespace AdmAPI.DAO
 
         public void CriarMeta(MetasDTO meta)
         {
+            var dateOnly =  meta.DATA_ALCANCAR.Date;
             var conexao = ConnectionFactory.Build();
             conexao.Open();
 
-            var query = @"INSERT INTO METAS (NOME_MT, VALOR_MT) VALUES
-            (@NOME_MT, @VALOR_MT);";
+            var query = @"INSERT INTO METAS (NOME_MT, VALOR_MT, DATA_ALCANCAR) VALUES
+            (@NOME_MT, @VALOR_MT, @DATA_ALCANCAR);";
 
             var comando = new MySqlCommand(query, conexao);
             comando.Parameters.AddWithValue("@NOME_MT", meta.NOME_MT);
             comando.Parameters.AddWithValue("@VALOR_MT", meta.VALOR_MT);
+            comando.Parameters.AddWithValue("@DATA_ALCANCAR", dateOnly);
 
             comando.ExecuteNonQuery();
             conexao.Close();
         }
         public void AlterarMeta(MetasDTO meta)
         {
+            var dateOnly = despesa.DATA_ALCANCAR.Date;
             var conexao = ConnectionFactory.Build();
             conexao.Open();
 
-            var query = @"UPDATE METAS SET NOME_MT = @NOME_MT, VALOR_MT = @VALOR_MT
+            var query = @"UPDATE METAS SET NOME_MT = @NOME_MT, VALOR_MT = @VALOR_MT, DATA_ALCANCAR = @DATA_ALCANCAR
             where COD_MT = @COD_MT";
 
             var comando = new MySqlCommand(query, conexao);
             comando.Parameters.AddWithValue("@COD_MT", meta.COD_MT);
             comando.Parameters.AddWithValue("@NOME_MT", meta.NOME_MT);
             comando.Parameters.AddWithValue("@VALOR_MT", meta.VALOR_MT);
+            comando.Parameters.AddWithValue("@DATA_ALCANCAR", dateOnly);
 
             comando.ExecuteNonQuery();
             conexao.Close();
--- API/AdmAPI/DTO/MetasDTO.cs
+++ API/AdmAPI/DTO/MetasDTO.cs
@@ -1,12 +1,15 @@
-﻿namespace AdmAPI.DTO
+﻿using System;
+
+namespace AdmAPI.DTO
 {
     public class MetasDTO
     { 
             public int COD_MT { get; set; }
             public double VALOR_MT { get; set; }
             public string NOME_MT { get; set; }
+        public DateTime DATA_ALCANCAR { get; set; }
 
-        }
+    }
     }
 
 
